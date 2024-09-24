e344f560e77f26f6e513649b226d424f802999fb
andaase <andaase@stud.ntnu.no>
2024-09-24 16:09:41 +0200

    New stuff

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" {
+    type = string
+    description = "(optional) describe your variable"
+}
+variable "rg_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
+variable "location" {
+    type = string
+    description = "(optional) describe your variable"
+}
