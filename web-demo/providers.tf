3d77b1a615c79075ce93bb6281bc3773acd1d226
andaase <andaase@stud.ntnu.no>
2024-09-25 09:14:32 +0200

    NEW: web-demo tested in terraform

---
+++ web-demo/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "aa-rg-backend-tfstate"
+    storage_account_name = "aasabetfsg8dafp3yfl"
+    container_name       = "aatfstate"
+    key                  = "web-demo.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  # Configuration options
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+
+  }
+  subscription_id = var.subscription_id
+}
---
+++ web-demo/variables.tf
@@ -0,0 +1,28 @@
+variable "subscription_id" {
+  type        = string
+  description = "(optional) describe your variable"
+  sensitive   = true
+}
+variable "rg_name" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+variable "location" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "sa_name" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "index_document" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "source_content" {
+  type        = string
+  description = "(optional) describe your variable"
+}
